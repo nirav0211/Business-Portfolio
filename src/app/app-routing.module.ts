1257925913b58945accbc10240e74433b85758ce
NIRAV PANDYA <npandy16@my.centennialcollege.ca>
2019-02-16 18:36:24 -0500

    Pages and Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServiceComponent } from './service/service.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  { path: 'aboutme', component:AboutmeComponent }, 
+  { path:'contactme', component:ContactmeComponent},
+  { path:'projects',component:ProjectsComponent},
+  { path:'services', component:ServiceComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+import { ServiceComponent } from './service/service.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    ContactmeComponent,
+    ServiceComponent,
+    ProjectsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
